b36b931394f05ae04450b344ca9810c4e89dfc06
Sahran <94926019+SahranREAL@users.noreply.github.com>
2024-11-22 21:20:04 +0100

    a

---
+++ assets/js/custom.js
@@ -0,0 +1,33 @@
+let index = 0;
+let imageCount = 0; // Compteur d'images passées
+let interval;
+
+function moveSlide(step) {
+  const slides = document.querySelectorAll('.carousel-item');
+  const totalSlides = slides.length;
+
+  index = (index + step + totalSlides) % totalSlides;
+  const offset = -index * (slides[0].offsetWidth + 15);
+  document.querySelector('.carousel').style.transform = `translateX(${offset}px)`;
+  
+  imageCount++; // Incrémente le compteur d'images passées
+  if (imageCount >= 10) { // Après 10 images passées, réinitialise l'animation
+    resetAnimation();
+  }
+}
+
+function autoSlide() {
+  moveSlide(1); // Déplace d'une image à la fois
+}
+
+function resetAnimation() {
+  // Réinitialise après 6 images
+  setTimeout(() => {
+    index = 0; // Réinitialise l'index à zéro
+    imageCount = 0; // Réinitialise le compteur d'images
+    moveSlide(0); // Réinitialise la position du carrousel
+  }, 1000); // Attends 1 seconde pour la réinitialisation
+}
+
+// Démarre l'animation initiale
+interval = setInterval(autoSlide, 3000);
